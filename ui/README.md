8d5a54c8c3717164790d54d74e233ca15896a05b
lovecrazy <630140621@qq.com>
2019-11-25 23:52:01 +0800

    增加前端vue常用组件库

---
+++ ui/README.md
@@ -0,0 +1,18 @@
+# 说明
+
+该文件夹下保存的是一些在cms项目中我通常使用的界面组件
+
+主要由以下几个框架组成:
+
+1. vue 2.x
+2. elementui 2.12.0
+3. font-awesome 5.x
+
+## 目录结构
+
+```
+.
+├── components
+│   └── lh-image.vue ── 图像组件+点击预览（elementui还支持懒加载等功能，此处并未添加）
+└── README.md
+```
---
+++ ui/components/lh-image.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- 
+    Usage:
+      src: ""
+      width: 100px —— 传入空字符串表示显示原图宽度
+      height: 100px —— 传入空字符串表示显示原图高度
+    
+    eg.
+      <lh-image src="http://qn-cover.feheadline.com/usup/20191115152153252545.gif" width="100px" height="100px"></lh-image>
+  -->
+  <div>
+    <img :src="src" :height="height" :width="width" @click="handleImageClick" />
+    <!-- 预览图片 -->
+    <el-image-viewer v-if="preview.display" :url-list="[src]" :on-close="handleClosePreview" />
+  </div>
+</template>
+
+<script>
+import ElImageViewer from "element-ui/packages/image/src/image-viewer";
+
+export default {
+  name: "lh-image",
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: "100px"
+    },
+    height: {
+      type: String,
+      default: "100px"
+    }
+  },
+  data() {
+    return {
+      image_style: {
+        width: this.width,
+        height: this.height
+      },
+      preview: {
+        display: false
+      }
+    };
+  },
+  methods: {
+    handleImageClick() {
+      this.preview.display = true;
+    },
+    handleClosePreview() {
+      this.preview.display = false;
+    }
+  },
+  components: {
+    ElImageViewer
+  }
+};
+</script>
